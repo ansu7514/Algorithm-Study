3fc3eefc8579e963340cede9ddea3d71dcfdc8e7
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2023-05-25 20:43:34 +0900

    [level 2] Title: 올바른 괄호, Time: 10.07 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,14 @@
 def solution(s):
     temp = list(s)
-    
+
     check = 0
     for i in range(len(temp)):
         if temp[i] == '(':
             check += 1
         else:
             check -= 1
-        
+
         if check < 0:
             return False
-            
+
     return True if check == 0 else False
