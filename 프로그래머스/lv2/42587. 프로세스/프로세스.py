49bffc6159eedcc31218adf6b29d668e121ddd93
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2023-07-30 14:15:16 +0900

    [level 2] Title: 프로세스, Time: 0.06 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+def solution(priorities, location):
+    queue = deque(priorities)
+    answer = 0
+
+    while queue:
+        m = max(queue)
+        l = queue.popleft()
+        location -= 1
+
+        if l != m:
+            queue.append(l)
+            if location < 0:
+                location = len(queue) -1
+        else:
+            answer += 1
+            if location < 0:
+                break
+
+    return answer
