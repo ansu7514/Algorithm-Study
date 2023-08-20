4b405bc0aaa10aa5726e8eaff2cf57261ad7858f
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2023-08-20 10:58:38 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,12 @@
+def solution(num_list, n):
+    answer = []
+    
+    temp = []
+    for i in range(len(num_list)):
+        temp.append(num_list[i])
+
+        if (i + 1) % n == 0:
+            answer.append(temp)
+            temp = []
+            
+    return answer
