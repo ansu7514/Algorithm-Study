8628c137be34c2e0493e60b54c349d36a83c0ac1
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2023-08-29 20:28:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,8 @@
+def solution(numbers):
+    numlist = { 'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4,
+                'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 }
+
+    for k, v in numlist.items():
+        numbers = numbers.replace(k, str(v))
+    
+    return int(numbers)
