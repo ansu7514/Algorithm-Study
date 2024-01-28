e372f2f796e14ac2886bf051aeba5ee1f4246964
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2024-01-28 23:09:40 +0900

    [Silver IV] Title: 암기왕, Time: 1524 ms, Memory: 222508 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,14 +1,14 @@
 t = int(input())
 
 for _ in range(t):
-  n = int(input())
-  note_1 = set(map(int, input().split()))
-  
-  m = int(input())
-  note_2 = list(map(int, input().split()))
+	n = int(input())
+	nList = set(map(int, input().split()))
 
-  for n2 in note_2:
-    if n2 in note_1:
-      print(1)
-    else:
-      print(0)
+	m = int(input())
+	mList = list(map(int, input().split()))
+
+	for num in mList:
+		if num in nList:
+			print(1)
+		else:
+			print(0)
